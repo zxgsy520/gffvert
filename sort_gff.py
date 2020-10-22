82a5ac56fdbd110304c221df3edcde7ba36ed5aa
sy520 <36355222+zxgsy520@users.noreply.github.com>
2020-10-22 11:46:01 +0800

    Add files via upload

--- sort_gff.py
+++ sort_gff.py
@@ -53,6 +53,8 @@ def get_id(attributes):
         gene_id = gene_attr['ID']
     elif 'Parent' in gene_attr:
         gene_id = gene_attr['Parent']
+    elif 'note' in gene_attr:
+        gene_id = gene_attr['note'].replace(' ', '')
     else:
         raise Exception('Gene has no name.')
 
@@ -64,7 +66,7 @@ def read_gff(file):
     gene_dict = {}
     mrna_dict = {}
     cds_dict = {}
-
+    n = 0
 
     for line in read_csv(file):
         gene_id = get_id(line[-1])
@@ -73,12 +75,17 @@ def read_gff(file):
             continue
         if line[2]=="mRNA":
             mrna_dict[gene_id] = line
+
         elif line[2]=='CDS':
             if gene_id not in cds_dict:
                 cds_dict[gene_id] = []
             cds_dict[gene_id].append(line)
-        else:
+        elif line[2] == 'gene':
             gene_id = gene_id.strip('-gene')
+            gene_dict[gene_id] = line
+        else:
+            n += 1
+            gene_id = "%s_%s" % (gene_id, n)
             gene_dict[gene_id] = line
 
     return gene_dict, mrna_dict, cds_dict
@@ -90,7 +97,7 @@ def update_gene_name(gene_line, old_name, locustag, number):
         new_name = '%s_%05d' % (locustag, number)
         gene_line[-1] = gene_line[-1].replace(old_name, new_name)
 
-    return '\t'.join(gene_line)
+    return new_name, '\t'.join(gene_line)
     
 
 def sort_gff(file, locustag):
@@ -99,18 +106,34 @@ def sort_gff(file, locustag):
     gene_dict, mrna_dict, cds_dict = read_gff(file)
 
     for line in sorted(gene_dict.items(), key = lambda x:(x[1][0], int(x[1][3]), int(x[1][4]))):
+        if line[1][2] not in ['gene', 'tRNA', 'rRNA']:
+            print('\t'.join(line[1]))
+            continue
+
         n += 1
-        print(update_gene_name(line[1], line[0], locustag, n))
+        new_name, gene_line = update_gene_name(line[1], line[0], locustag, n)
+        print('%s;Name=%s;' % (gene_line, new_name))
+
+        if line[1][2]!='gene':
+            continue
 
         if line[0] in mrna_dict:
-            print(update_gene_name(mrna_dict[line[0]], line[0], locustag, n))
+            new_name, mrna_line = update_gene_name(mrna_dict[line[0]], line[0], locustag, n)
+            print(mrna_line)
+        else:
+            line[1][2] = 'mRNA'
+            new_name, mrna_line = update_gene_name(line[1], line[0], locustag, n)
+            print('%s;Name=%s;Parent=%s;' % (mrna_line, new_name, new_name))
 
-        if line[0] not in  cds_dict:
+        if line[0] not in cds_dict:
             continue
         for cdsline in sorted(cds_dict[line[0]], key = lambda x: (x[0], int(x[3]), int(x[4]))):
-            cds_line = update_gene_name(cdsline, line[0], locustag, n)
-            cdsline[2] = "exon"
-            print(update_gene_name(cdsline, line[0], locustag, n))
+            new_name, cds_line = update_gene_name(cdsline, line[0], locustag, n)
+
+            if len(cds_dict[line[0]])>=2:
+                cdsline[2] = "exon"
+                new_name, exon_line = update_gene_name(cdsline, line[0], locustag, n)
+                print(exon_line)
             print(cds_line)
 
     return 0
